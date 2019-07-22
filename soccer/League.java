d0b6953438f840929fe37bbba963356051fb888d
yegan <yeganj@gmail.com>
2019-07-22 18:04:13 +0200

    practice8

--- soccer/League.java
+++ soccer/League.java
@@ -1,7 +1,12 @@
 package soccer;
 
 public class League{
+
     public static void main(String[] args){
+        Team[] theTeams = createTeams();
+        Game[] theGames = createGames(theTeams);
+
+       
         Player player1 = new Player();
         player1.playerName = "George Eliot";
 
@@ -12,29 +17,20 @@ public class League{
         player3.playerName = "Geoffrey Chaucer";
 
         Player[] thePlayers = {player1, player2, player3};
-
-        Team team1 = new Team();
-        team1.teamName = "The Greens";
-        team1.playerArray = thePlayers;
-
-        Team team2 = new Team();
-        team2.teamName = "The Reds";
-        team2.playerArray = new Player[3];
-
-        team2.playerArray[0]= new Player();
-        team2.playerArray[0].playerName = "Robbie Service";
-
-        team2.playerArray[1]= new Player();
-        team2.playerArray[1].playerName = "Robbie Burns";
-
-        team2.playerArray[2] = new Player();
-        team2.playerArray[2].playerName = "Rafeal Sabatini";
         
 
-        Game currGame = new Game();
-        currGame.homeTeam = team1;
-        currGame.awayTeam = team2;
+        public static Team[] createTeams(){
+        Team[] theTeams = {team1, team2};
+        return theTeams;
 
+        }
+
+        public static Game[] createGames(Team[] theTeams){
+            Game currGame = theGames[0];
+            Game[] theGames = {theGame};
+            return theGames;
+        }
+      
 
         Goal goal1 = new Goal();
         goal1.thePlayer = currGame.homeTeam.playerArray[2];
@@ -45,14 +41,9 @@ public class League{
         currGame.goals = theGoals;
 
         System.out.println("Goal scored after "+ currGame.goals[0].theTime + " mins by "+ currGame.goals[0].thePlayer.playerName + " of "+ currGame.goals[0].theTeam.teamName);
-        // for(Player thePlayer: team1.playerArray){
-        //     System.out.println(thePlayer.playerName);
-        // }
-
-        // for(Player thePlayer: team2.playerArray){
-        //     System.out.println(thePlayer.playerName);
-        // }
-
+        
 
     }
+
+    
 }
